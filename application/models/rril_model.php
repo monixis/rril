74f7b93c7047683eda0f649c4e73fd67528cce42
Weon Yuan <weon.yuan@gmail.com>
2015-01-28 15:59:20 -0500

    Modified tables in database, and the SQL queries.
    Modified in order to expand the database and to maintain functionality
    of the website.

--- application/models/rril_model.php
+++ application/models/rril_model.php
@@ -16,10 +16,10 @@ class rril_model extends CI_Model {
 	function getbooks($cid) {
 
 		if ($cid == 0) {
-			$sql = "SELECT DISTINCT books.isbn, books.title, books.author, books.publishername, books.publishedyear, scores.maristcoeff as 'weightedaverage' FROM scores INNER JOIN books ON scores.isbn = books.isbn ORDER BY scores.maristcoeff DESC LIMIT 200";
+			$sql = "SELECT DISTINCT booksISBN.isbn, books.title, books.author, books.publishername, books.publishedyear, scores.maristcoeff as 'weightedaverage' FROM scores INNER JOIN booksISBN ON scores.isbn = booksISBN.isbn INNER JOIN books ON booksISBN.bid = books.bid ORDER BY scores.maristcoeff DESC LIMIT 200";
 			$results = $this -> db -> query($sql);
 		} else {
-			$sql = "SELECT DISTINCT books.isbn, books.title, books.author, books.publishername, books.publishedyear, scores.maristcoeff as 'weightedaverage' FROM scores INNER JOIN books ON scores.isbn = books.isbn WHERE books.category = ? ORDER BY scores.maristcoeff DESC LIMIT 200";
+			$sql = "SELECT DISTINCT booksISBN.isbn, books.title, books.author, books.publishername, books.publishedyear, scores.maristcoeff as 'weightedaverage' FROM scores INNER JOIN booksISBN ON scores.isbn = booksISBN.isbn INNER JOIN books ON booksISBN.bid = books.bid WHERE books.category = ? ORDER BY scores.maristcoeff DESC LIMIT 200";
 			$results = $this -> db -> query($sql, array($cid));
 		}
 
@@ -27,19 +27,19 @@ class rril_model extends CI_Model {
 	}
 
 	function getbookdetails($isbn) {
-		$sql = "SELECT books.isbn, books.title, books.subtitle, books.author, books.publishername, books.publishedyear, scores.maristcoeff, scores.sales, scores.citation FROM books INNER JOIN scores ON books.isbn = scores.isbn WHERE books.isbn= ?";
+		$sql = "SELECT booksISBN.isbn, books.title, books.subtitle, books.author, books.publishername, books.publishedyear, scores.maristcoeff, scores.sales, scores.citation FROM books INNER JOIN booksISBN ON books.bid = booksISBN.bid INNER JOIN scores ON booksISBN.isbn = scores.isbn WHERE booksISBN.isbn= ?";
 		$results = $this -> db -> query($sql, array($isbn));
 		return $results -> result();
 	}
 
 	function getfeaturedbooks() {
-		$sql = "SELECT DISTINCT books.isbn, books.title, scores.maristcoeff, books.coverlink, books.author FROM books INNER JOIN scores ON books.isbn = scores.isbn ORDER BY scores.maristcoeff DESC LIMIT 50";
+		$sql = "SELECT DISTINCT booksISBN.isbn, books.title, scores.maristcoeff, books.coverlink, books.author FROM books INNER JOIN booksISBN ON books.bid = booksISBN.bid INNER JOIN scores ON booksISBN.isbn = scores.isbn ORDER BY scores.maristcoeff DESC LIMIT 50";
 		$results = $this -> db -> query($sql);
 		return $results -> result();
 	}
 	
 	function getbooksbycategory($qry){
-		$sql = "SELECT DISTINCT books.isbn, books.title, scores.maristcoeff, books.coverlink, books.author FROM books INNER JOIN scores ON books.isbn = scores.isbn INNER JOIN bookcategory on books.isbn = bookcategory.isbn WHERE ";
+		$sql = "SELECT DISTINCT booksISBN.isbn, books.title, scores.maristcoeff, books.coverlink, books.author FROM books INNER JOIN booksISBN ON books.bid = booksISBN.bid INNER JOIN scores ON booksISBN.isbn = scores.isbn INNER JOIN booksCategories on books.bid = booksCategories.bid WHERE ";
 		$sql = $sql . $qry;
 		$results = $this -> db -> query($sql);
 		return $results -> result();
